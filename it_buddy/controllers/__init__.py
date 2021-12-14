9f62b21fdea95c035ceadb4b2d9e28edec94d109
andrewabsa <farshadabsalan6@gmail.com>
2021-12-14 22:47:29 +1100

    added user controller and templates

--- it_buddy/controllers/__init__.py
+++ it_buddy/controllers/__init__.py
@@ -1,4 +1,5 @@
 from controllers.course_controller import courses
+from controllers.user_controller import users
 
-registerable_controllers = [courses]
+registerable_controllers = [courses, users]
 
--- it_buddy/main.py
+++ it_buddy/main.py
@@ -3,9 +3,11 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from marshmallow.exceptions import ValidationError
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 ma = Marshmallow()
+lm = LoginManager()
 
 def create_app():
 
@@ -16,6 +18,7 @@ def create_app():
 
     db.init_app(app)
     ma.init_app(app)
+    lm.init_app(app)
 
     from commands import db_commands
     app.register_blueprint(db_commands)
